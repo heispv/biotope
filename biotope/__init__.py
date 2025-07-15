45aab971bbe8254a69620dc3c828bcaddab03bcd
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2025-07-15 23:42:30 +0200

    Bump version: 0.3.1 → 0.3.2

--- biotope/__init__.py
+++ biotope/__init__.py
@@ -4,4 +4,4 @@ from biotope.cli import cli
 
 __all__ = ["cli"]
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
--- biotope/cli.py
+++ biotope/cli.py
@@ -18,7 +18,7 @@ from biotope.commands.status import status as status_cmd
 
 
 @click.group()
-@click.version_option(version="0.3.1")
+@click.version_option(version="0.3.2")
 @click.pass_context
 def cli(ctx: click.Context) -> None:
     """CLI entrypoint."""
